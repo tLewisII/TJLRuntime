af9f3bd1c8694fb7a00300f55913ec3293c59dde
Terry Lewis <terrylewis@outlook.com>
2013-12-25 19:21:14 -0600

    Initial Commit

---
+++ TJLRuntime/TJLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TJLAppDelegate.h
+//  TJLRuntime
+//
+//  Created by Terry Lewis II on 12/25/13.
+//  Copyright (c) 2013 Terry Lewis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TJLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TJLRuntime/TJLViewController.h
@@ -0,0 +1,13 @@
+//
+//  TJLViewController.h
+//  TJLRuntime
+//
+//  Created by Terry Lewis II on 12/25/13.
+//  Copyright (c) 2013 Terry Lewis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TJLViewController : UIViewController
+
+@end

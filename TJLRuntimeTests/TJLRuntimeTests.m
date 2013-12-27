130fc0accf618b4beff2232311bed005a0c22491
Terry Lewis <terrylewis@outlook.com>
2013-12-26 19:03:51 -0600

    Added more tests for properties.

--- TJLRuntimeTests/TJLRuntimeTests.m
+++ TJLRuntimeTests/TJLRuntimeTests.m
@@ -15,6 +15,7 @@
     TJLTestClass *_testClass;
     NSArray *_propertyNames;
     NSArray *_propertyTypes;
+    NSArray *_propertyIvarValues;
 }
 
 @end
@@ -27,6 +28,7 @@
     _runtime = [[TJLRuntime alloc]init];
     _propertyNames = @[@"nameString", @"number"];
     _propertyTypes = @[@"NSString", @"NSNumber"];
+    _propertyIvarValues = @[@"Terry", @42];
 }
 
 - (void)tearDown {
@@ -55,5 +57,35 @@
         NSString *staticType = _propertyTypes[i];
         XCTAssertTrue([runtimeProperty.type isEqualToString:staticType], @"Types should be equal");
     }
+}
+
+- (void)test_property_dictionary {
+    NSDictionary *dictionary = [_runtime propertyTypeAndNameDictionaryForClass:_testClass.class];
+    __block NSInteger i = 0;
+    [dictionary enumerateKeysAndObjectsUsingBlock:^(NSString *keyName, NSString *valueType, BOOL *stop) {
+        NSString *type = _propertyTypes[i];
+        NSString *name = _propertyNames[i];
+        
+        XCTAssertTrue([type isEqualToString:valueType], @"Types should be equal");
+        XCTAssertTrue([name isEqualToString:keyName], @"Names should be equal");
+        i++;
+    }];
+}
+
+- (void)test_property_ivar_value {
+    NSArray *properties = [_runtime propertiesForClass:_testClass.class withInstance:_testClass];
+    XCTAssertTrue(properties.count == _propertyIvarValues.count, @"Count should be the same");
+    
+    TJLProperty *stringProperty = properties.firstObject;
+    TJLProperty *numberProperty = properties.lastObject;
+    
+    XCTAssertNotNil(stringProperty.ivar, @"Should not be nil");
+    XCTAssertNotNil(numberProperty.ivar, @"Should not be nil");
+    
+    XCTAssertTrue(stringProperty.ivar.instance == _testClass, @"Should be pointer equal");
+    XCTAssertTrue(numberProperty.ivar.instance == _testClass, @"Should be pointer equal");
+    
+    XCTAssertTrue([stringProperty.ivar.value compare:_propertyIvarValues.firstObject] == NSOrderedSame, @"Strings should be the same");
+    XCTAssertTrue([numberProperty.ivar.value compare:_propertyIvarValues.lastObject] == NSOrderedSame, @"Numbers should be the same");
 }
 @end
